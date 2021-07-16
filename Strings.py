b7606888c6ad2bb389ae8c8db3e7b6dc25e20e99
david757 <david-valkenburg@hotmail.com>
2021-07-16 13:57:13 +0200

    Update Strings.py
    Verbeterde versie

--- Strings.py
+++ Strings.py
@@ -3,25 +3,29 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
+# Part 1
+scorer_name_0 = 'Marco van Basten'
+goal_0 = 32
+scorer_name_1 = 'Ruud Gullit'
+goal_1 = 54
 
 
-scorer_name_1 = 'Marco van Basten'
-goal_1 = '32th'
-scorer_name_2 = 'Ruud Gullit'
-goal_2 = '54th'
+scorers = (str(scorer_name_0) + ' ' + str(goal_0) + 'nd')+', '+ (str(scorer_name_1) +' ' + str(goal_1) + 'th')
+print(scorers)
 
+report =(f'{scorer_name_0} scored in the {goal_0}nd minute \n{scorer_name_1} scored in the {goal_1}th minute')
+print(report)
 
-scorers = ( scorer_name_1 + goal_1 ), ( scorer_name_2 + goal_2 )
-
-report =(f'{scorer_name_1} scored in the {goal_1} minute' '\n' f'{scorer_name_2} scored in the {goal_2} minute')
-
+# Part 2
 player = 'Ruud Gullit'
 
-first_name = player[0:4]
-last_name_len = len(player[5:11])
-name_short = (player[:1]) + ('. ') + (player[5:11])
+first_name = player[0:player.find(' ')]
+last_name_len = len(player[player.find(' ')+1 :])
 
-chant = first_name + (last_name_len * '!')
+name_short = (player[:1]) + ('. ') + (player[player.find(' ')+1:])
 
-good_chant = chant[12:] != ' '
+chant = (first_name + '!') * len(first_name)
+print(chant)
+
+good_chant = chant[-1:] != ' '
 print(good_chant)
